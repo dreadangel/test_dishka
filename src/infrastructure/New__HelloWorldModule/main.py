3e05129a654986def5630c7c1ba3d96d7cb184c7
morington <morington.mail@gmail.com>
2024-07-20 02:25:54 +0300

    upd

--- src/infrastructure/New__HelloWorldModule/main.py
+++ src/infrastructure/New__HelloWorldModule/main.py
@@ -2,8 +2,8 @@ from src.core.ports.helloworldport import HelloWorldInterface
 
 
 class NewHelloWorld(HelloWorldInterface):
-    def __init__(self) -> None:
-        self.text = 'Hello world! New module'
+    def __init__(self, addt_text: str) -> None:
+        self.text = f'Hello world! New module {addt_text}'
 
     async def hello_world(self) -> None:
         print(self.text)
